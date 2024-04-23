3a6966709b5e0151cd875f0a5353e39b0203730a
georginaelena <georgina.elena@ui.ac.id>
2024-04-24 00:42:41 +0700

    Change NPM format.

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
 
         // thread::sleep(ten_millis);
 
-        println!("In 2206810995-Elena's Computer [129500004y]. Message received: {:?}", message);
+        println!("In Elena's Computer [2206810995]. Message received: {:?}", message);
         Ok(())
     }
 
